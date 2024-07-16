0618304c66b31ce406e67a0a11ead439af826920
DFrancs <dkf55@nau.edu>
2024-07-15 23:49:44 -0700

    Adding homework files

---
+++ Exercise 1/src/notes.js
@@ -0,0 +1,20 @@
+// notes.js
+const notes = [
+  {
+    key: 1,
+    title: "Note 1",
+    content: "Here's note 1",
+  },
+  {
+    key: 2,
+    title: "Note 2",
+    content: "This is note 2",
+  },
+  {
+    key: 3,
+    title: "Note 3",
+    content: "And of course, here is note 3",
+  },
+];
+
+export default notes;
